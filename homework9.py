bdcc3ce3935e8eca6fadc8d671bbec3456e5b200
danon2112 <d.ivanov.d2112@gmail.com>
2024-06-27 01:45:39 +0300

    update files: цикл For

--- homework9.py
+++ homework9.py
@@ -1,11 +1,19 @@
+
+print('\n\n\n\n\n\n\n\n\n\n')
+
+
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = list()
 not_primes = list()
 
 
+
 for i in numbers:
     if i == 1:
         continue
+    if i == 2:
+        primes.append(i)
+
 
     for j in range(2, i):
         is_primes = (i % j) != 0
