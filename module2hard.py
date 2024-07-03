4ace1c1dce481d6f6bc467565cdfab510bdc3867
danon2112 <d.ivanov.d2112@gmail.com>
2024-07-03 18:38:12 +0300

    module2hard.py update

--- module2hard.py
+++ module2hard.py
@@ -4,7 +4,7 @@ if n > 20 or n < 3:
     print(f'Ошибка! Число должно быть от 3 до 20.')
 else:
     for i in range(1, n):
-        for j in range(i, n):
+        for j in range(i + 1, n):
             if (n % (i + j)) == 0:
                 password = f'{password}{i}{j}'
 
