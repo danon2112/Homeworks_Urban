b968f5fea64cf9e40146b9aee17d3a12f7e99d4d
danon2112 <d.ivanov.d2112@gmail.com>
2024-07-05 15:02:17 +0300

    module_3_3.py uploaded

--- module_3_2.py
+++ module_3_2.py
@@ -1,4 +1,4 @@
-
+name = 'Daniil'
 
 def send_email(message, recipient, *, sender='university.help@gmail.com'):
 
@@ -36,4 +36,6 @@ def send_email(message, recipient, *, sender='university.help@gmail.com'):
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Пожалуйста, исправьте задание', 'urban.studentmail.ru', sender='urban.teachermail.ru')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
