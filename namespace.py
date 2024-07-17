d0ed0688e5ddb37a4a421a14eed28685c1868c52
danon2112 <d.ivanov.d2112@gmail.com>
2024-07-17 12:59:09 +0300

    namespace.py upload

---
+++ namespace.py
@@ -0,0 +1,8 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+        return
+    inner_function()
+    return
+test_function()
+# inner_function() - Ошибка: NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
